2ab0e3f116e728266f00655561fde493371ba633
-沉着冷静爱自由 <1064856783@qq.com>
2018-11-29 16:35:44 +0800

    逻辑优化

--- src/main/java/test/QueryHLR.java
+++ src/main/java/test/QueryHLR.java
@@ -6,17 +6,22 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.LineNumberReader;
 import java.io.PrintStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class QueryHLR {
-	
+    private static final Log logger = LogFactory.getLog(QueryHLR.class);
+    
 	public static void main(String[] args) throws Exception {
 		/*
 		 * System.out.println("调用["+Thread.currentThread().getStackTrace()[1].
@@ -24,11 +29,14 @@ public class QueryHLR {
 		 * stackTrace=Thread.currentThread().getStackTrace(); new Test06().test();
 		 */
 		InputStream read = new FileInputStream("E:\\new.txt");
+		List<String> resultList = new ArrayList<String>();
+		List<String> errorList = new ArrayList<String>();
+		String[] readFileLines = readLineStream(read);
+		List<String> readFileLinesList =new ArrayList<String>(Arrays.asList(readFileLines));
+		int orginNum = readFileLinesList.size();
+		//重试次数
+		int reTryNum = 10;
 		try {
-			String[] readFileLines = readLineStream(read);
-			
-			List<String> readFileLinesList =new ArrayList<String>(Arrays.asList(readFileLines));
-			
 			/*
 			 * List<String> stringss = Arrays.asList(ss); Collections.sort(stringss,new
 			 * Comparator<String>() {
@@ -39,13 +47,15 @@ public class QueryHLR {
 			 */
 			HttpClient client = new HttpClient();
 			// 设置超时时间
-			client.getHttpConnectionManager().getParams().setConnectionTimeout(10 * 1000);
-			client.getHttpConnectionManager().getParams().setSoTimeout(10 * 1000);
+			client.getHttpConnectionManager().getParams().setConnectionTimeout(reTryNum * 1000);
+			client.getHttpConnectionManager().getParams().setSoTimeout(reTryNum * 1000);
 			// 设置参数
 //			StringBuilder tempList = new StringBuilder();
-			List<String> resultList = new ArrayList<String>();
 //			long startTime = System.currentTimeMillis(); System.currentTimeMillis()-startTime > 300000//最多查询5分钟
-			while (readFileLinesList.size() >= 1) {
+			int curReTryNum = 0;
+			while (readFileLinesList.size() >= 1&&curReTryNum<=reTryNum) {
+				System.out.println("第"+curReTryNum+"次重试,剩余数量:"+readFileLinesList.size());
+				curReTryNum++;
 //				System.out.println("While!!");
 				for (int i = 0; i < readFileLinesList.size(); i++) {
 //					System.out.println("For!!");
@@ -56,21 +66,40 @@ public class QueryHLR {
 					 * }
 					 */
 					String hlrIn = readFileLinesList.get(i).trim();
+//					System.out.println("QueryLog:"+hlrIn);
 					String[] arr = hlrIn.split(":");
 					if (hlrIn.length() > 7 && arr.length >= 2 && StringUtils.isNotBlank(arr[1])) {
 						readFileLinesList.remove(i);
 						String temp = "INSERT INTO `yyvoinvdb1`.`bds_mobile_prefix`\r\n"
 								+ "VALUES ('"+arr[0]+"0000', '"+arr[0]+"9999', '"+arr[1]+"', "
 								+ "'*', '1', '7', '0', '2001-01-01 00:00:00', '2037-01-01 00:00:00', '');";
-						
 						resultList.add(temp);
 						continue;
 					}
-					hlrIn =hlrIn.replaceAll("[:]", "");
+					hlrIn = hlrIn.replaceAll("[:]", "");
 					GetMethod getMethod = new GetMethod("http://www.ip138.com:8080/search.asp?action=mobile&mobile=" + hlrIn);
 					getMethod.setRequestHeader("CharSet", "GBK");
-					client.executeMethod(getMethod);
-					String retHtml = new String(getMethod.getResponseBodyAsString().getBytes("ISO-8859-1"), "gb2312");
+					try {
+						client.executeMethod(getMethod);
+						String retHtml = new String(getMethod.getResponseBodyAsString().getBytes("ISO-8859-1"), "gb2312");
+						String sIndex = "<TR class=tdc bgcolor=#EFF1F3>\r\n" 
+										+ "		<TD align=\"center\">区 号</TD>\r\n"
+										+ "		<TD align=\"center\" class=tdc2>";
+						int sNum = retHtml.indexOf(sIndex) + sIndex.length();
+						String region = retHtml.substring(sNum, sNum + 4).replaceAll("[iv=\"|<|/TD]", "");
+						if(StringUtils.isNotBlank(region)) {
+							readFileLinesList.remove(i);
+							String temp = "INSERT INTO `yyvoinvdb1`.`bds_mobile_prefix`\r\n"
+									+ "VALUES ('"+hlrIn+"0000', '"+hlrIn+"9999', '"+region+"', "
+									+ "'*', '1', '7', '0', '2001-01-01 00:00:00', '2037-01-01 00:00:00', '');";
+							resultList.add(temp);
+						}
+					} catch (Exception e) {
+						System.err.println("ErrorNum:"+hlrIn+"\nErrorLog:"+e.getMessage());
+						errorList.add(hlrIn);
+					}
+					
+					/*String retHtml = new String(getMethod.getResponseBodyAsString().getBytes("ISO-8859-1"), "gb2312");
 					String sIndex = "<TR class=tdc bgcolor=#EFF1F3>\r\n" 
 									+ "		<TD align=\"center\">区 号</TD>\r\n"
 									+ "		<TD align=\"center\" class=tdc2>";
@@ -83,32 +112,45 @@ public class QueryHLR {
 								+ "'*', '1', '7', '0', '2001-01-01 00:00:00', '2037-01-01 00:00:00', '');";
 						resultList.add(temp);
 					}
-					
+					*/
 				}
 			}
+			System.out.println("-----------------------");
+			System.out.println("查询结束,剩余数量:"+readFileLinesList.size());
 			if (readFileLinesList.size() > 0) {
-				resultList.addAll(readFileLinesList);
+				errorList.addAll(readFileLinesList);
 			}
 //			String[] sda = tempList.toString().split("#");
 //			List<String> stringB = Arrays.asList(sda);
 			//JDK1.7以后Array.sort优化了legacyMergeSort(传统归并排序) 改用 Timsort
-			System.setProperty("java.util.Arrays.useLegacyMergeSort", "true");
+			/*System.setProperty("java.util.Arrays.useLegacyMergeSort", "true");
 			Collections.sort(resultList, new Comparator<String>() {
 				@Override
 				public int compare(String o1, String o2) {
-					if (o1.length() > o2.length())
+					if (StringUtils.equals(o1, o1))
 						return -1;
 					else
 						return 1;
 				}
-			});
-			System.out.println(resultList.toString());
-			writeFile("E:\\result.txt", resultList);
-			System.out.println("查询完成！");
+			});*/
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
+			System.out.println("-----------------------");
+			System.out.println("总共数量："+orginNum);
+			System.out.println("-----------------------");
+			System.out.println("完成数量："+resultList.size());
+			System.out.println("-----------------------");
+			System.out.println("失败数量："+errorList.size());
+			System.out.println("-----------------------");
+			SimpleDateFormat sf = new SimpleDateFormat("yyyyMMddHH24mmss");
+//			String s =sf.format(new Date(System.currentTimeMillis()));
+			writeFile("E:\\result_"+sf.format(new Date(System.currentTimeMillis()))+".txt", resultList);
+			if (errorList.size()>0) {
+				writeFile("E:\\error_"+sf.format(new Date(System.currentTimeMillis()))+".txt", errorList);
+			}
+			System.out.println("查询完成！");
 			read.close();
 		}
 
