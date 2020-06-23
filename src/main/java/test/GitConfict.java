f7caf74b3e926f4665bb24be2cc06383e56e6e3f
-沉着冷静爱自由 <1064856783@qq.com>
2020-06-23 11:07:26 +0800

    Update GitConfict.java

--- src/main/java/test/GitConfict.java
+++ src/main/java/test/GitConfict.java
@@ -4,7 +4,10 @@ public class GitConfict {
 	
 	public static void main(String[] args) {
 		System.out.println("本地修改的");
-		System.out.println("git修改的");
+		
+		System.out.println("remote修改的");
+		
+		System.err.println("remote add");
 	}
 
 }
