cfad7591ee0a4a205d7fbf9a94714adf607fc00f
Najae <1064856783@qq.com>
2020-06-23 11:07:45 +0800

    local commit

--- src/main/java/test/GitConfict.java
+++ src/main/java/test/GitConfict.java
@@ -3,8 +3,11 @@ package test;
 public class GitConfict {
 	
 	public static void main(String[] args) {
-		System.out.println("本地修改的");
+		System.out.println("local修改的");
+		
 		System.out.println("git修改的");
+		
+		System.err.println("local add");
 	}
 
 }
